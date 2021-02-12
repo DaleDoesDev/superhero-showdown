ef442524df8b934d11c70c5f8773d57ee099f79d
DaleDoesDev <bumgardner.dale@gmail.com>
2021-02-12 00:03:33 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,5 @@
+let express = require('express'), app = express()
+app.use(express.static(__dirname + '/public')) 
+let PORT = process.env.PORT || 3000;
+app.listen(PORT)
+console.log(`App running on localhost port: ${PORT}.`)
